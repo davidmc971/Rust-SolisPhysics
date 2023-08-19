c2c9f7acc56f0250764e49f39e7510bbafe7db2a
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2023-08-19 23:38:10 +0200

    Math bindings done, submodule updated

--- src/math.rs
+++ src/math.rs
@@ -12,12 +12,15 @@ pub trait Isometry2DFunctions {
 
 pub trait Vec2Functions {
     fn normalize(&mut self);
-    fn rotate(&mut self, rotation: Vec2);
+    fn rotate(&mut self, rotation: &Vec2);
+    fn rotate_rad(&mut self, radians: Real);
     fn scale(&mut self, scalar: Real);
-    fn dot(a: Vec2, b: Vec2) -> Real;
-    fn add(&mut self);
-    fn sub(&mut self);
-    fn mul_add(&mut self);
+    fn dot(a: &Vec2, b: &Vec2) -> Real;
+    fn add(&mut self, other: &Vec2);
+    fn sub(&mut self, other: &Vec2);
+    fn mul_add(&mut self, other: &Vec2, scalar: Real);
+    fn length(vec: &Vec2) -> Real;
+    fn length_sq(vec: &Vec2) -> Real;
 }
 
 impl Isometry2DFunctions for Isometry2D {
@@ -36,4 +39,53 @@ impl Isometry2DFunctions for Isometry2D {
             Sol_Isometry2DSub(self, other);
         }
     }
+}
+
+impl Vec2Functions for Vec2 {
+    fn normalize(&mut self) {
+        unsafe { Sol_Vec2Normalize(self) }
+    }
+
+    fn rotate(&mut self, rotation: &Vec2) {
+        unsafe { Sol_Vec2Rotate(self, rotation) }
+    }
+
+    fn rotate_rad(&mut self, radians: Real) {
+        unsafe { Sol_Vec2RotateRad(self, radians) }
+    }
+
+    fn scale(&mut self, scalar: Real) {
+        unsafe { Sol_Vec2Scale(self, scalar) }
+    }
+
+    fn dot(a: &Vec2, b: &Vec2) -> Real {
+        unsafe { Sol_Vec2Dot(a, b) }
+    }
+
+    fn add(&mut self, other: &Vec2) {
+        unsafe { Sol_Vec2Add(self, other) }
+    }
+
+    fn sub(&mut self, other: &Vec2) {
+        unsafe { Sol_Vec2Sub(self, other) }
+    }
+
+    fn mul_add(&mut self, other: &Vec2, scalar: Real) {
+        unsafe { Sol_Vec2MulAdd(self, other, scalar) }
+    }
+
+    fn length(vec: &Vec2) -> Real {
+        unsafe { Sol_Vec2Length(vec) }
+    }
+
+    fn length_sq(vec: &Vec2) -> Real {
+        unsafe { Sol_Vec2Length2(vec) }
+    }
+}
+
+pub fn min(a: Real, b: Real) -> Real {
+    unsafe { Sol_Min(a, b) }
+}
+pub fn max(a: Real, b: Real) -> Real {
+    unsafe { Sol_Max(a, b) }
 }

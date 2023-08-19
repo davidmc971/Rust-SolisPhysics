bb68256b309cd1afde2543280853bfded7dbd2b9
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2023-08-19 19:02:43 +0200

    static link build, moved artifacts to OUT_DIR

--- build.rs
+++ build.rs
@@ -1,14 +1,19 @@
-use std::{fs, path::PathBuf};
+use std::{env, path::PathBuf};
 
 use bindgen::CargoCallbacks;
 
 fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let c_build = PathBuf::from(&out_dir).join("c_build");
+
     // Create meson build files
     if !std::process::Command::new("meson")
         .arg("setup")
-        .arg("c_build")
+        .arg(&c_build)
         .arg("SolisPhysics")
         .arg("--wipe")
+        .arg("--default-library")
+        .arg("static")
         .output()
         .expect("could not spawn `meson`")
         .status
@@ -21,7 +26,7 @@ fn main() {
     // Run ninja to compile
     if !std::process::Command::new("ninja")
         .arg("-C")
-        .arg("c_build")
+        .arg(&c_build)
         .output()
         .expect("could not spawn `meson`")
         .status
@@ -30,6 +35,12 @@ fn main() {
         // Panic if the command was not successful.
         panic!("could not generate meson build files");
     }
+
+    let linker_path = &c_build.canonicalize().expect("Cannot canonicalize path.");
+
+    // Tell rust to link our lib
+    println!("cargo:rustc-link-search={}", &linker_path.to_str().unwrap());
+    println!("cargo:rustc-link-lib=solis_physics");
 
     // Generate bindings
     let bindings = bindgen::Builder::default()
@@ -42,9 +53,8 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
-    let _ = fs::create_dir_all("bindgen");
-    let out_path = PathBuf::from("bindgen/bindgen_ffi_bindings.rs");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
-        .write_to_file(out_path)
+        .write_to_file(out_path.join("bindgen_ffi_bindings.rs"))
         .expect("Could not write bindings to file.");
 }
